c93aad4febd285542da578f59161d17af3ff3b3f
Margarita Katsarska <maggie.itfairy@gmail.com>
2013-05-15 21:34:31 +0300

    Add Class PersonInScoreboard.

---
+++ Hangman-3/PersonInScoreboard.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HangmanGame
+{
+    public class PersonInScoreboard: IComparable<PersonInScreboard>
+    {
+        private string name;
+
+       public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+        private set
+        {
+            if (value == null)
+            {
+                throw new NullReferenceException("Name can not be NULL!!!");
+            }
+            this.name = value;
+        }
+    }
+       private int mistakeNumber;
+
+       public int MistakeNumber
+       {
+           get
+           {
+               return this.mistakeNumber;
+           }
+           private set
+           {
+               this.mistakeNumber = value;
+           }
+       }
+
+        public int CompareTo(PersonInScreboard other)
+        {
+            return this.mistakeNumber.CompareTo(other.mistakeNumber);
+            throw new NotImplementedException();
+        }
+    }
+}
--- Hangman-3/Scoreboard.cs
+++ Hangman-3/Scoreboard.cs
@@ -7,6 +7,48 @@ namespace HangmanGame
 {
     class Scoreboard
     {
+       private readonly int TopRecordsNumber = 5;
 
+        private QalifiesForScoreboard()
+        {
+
+        }
+
+
+        
+
+        private AddInScoreboard()
+        {
+          PersonInScoreBoard.name = Console.ReadLine(); 
+        }
+
+        static void printboard(Dictionary<string, int> score)
+    {
+        if (score.Count == 0)
+        {
+            Console.WriteLine("Empty Scoreboard!");
+            return;
+        }
+
+        List<KeyValuePair<string, int>> key = new List<KeyValuePair<string, int>>();
+        foreach (var item in score)
+        {
+            KeyValuePair<string, int> current = new KeyValuePair<string, int>(item.Key, item.Value);
+            key.Add(current);
+        }
+
+        key.Sort(new OutComparer());
+        Console.WriteLine("Scoreboard:");
+        for (int i = 0; i < score.Count; i++)
+        {
+            Console.WriteLine("{0}. {1} --> {2} mistake", i + 1, key[i].Key, key[i].Value);
+            if (i == 4)
+            {              
+                break;
+            }
+        }
+
+        Console.WriteLine();
+    }
     }
 }
