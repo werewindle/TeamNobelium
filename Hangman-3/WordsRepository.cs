f967413e41d36a1280345b7baa4357c52a8dfe09
ms-nenova <ms.nenova@gmail.com>
2013-05-15 13:57:26 +0300

    Add class WordsRepository
    Sample format

---
+++ Hangman-3/WordsRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HangmanGame
+{
+    public class WordsRepository
+    {
+        private readonly string[] wordsCollection =
+        {
+            "computer",
+            "programmer",
+            "software",
+            "debugger",
+            "compiler",
+            "developer",
+            "algorithm",
+            "array",
+            "method",
+            "variable"
+        };
+
+        readonly Random randomWord = new Random();
+        private readonly string theChosenWord = wordsCollection[randomWord.Next(0, 10)];
+        private readonly int lengthOfTheWord = theChosenWord.Length;
+        private readonly char[] secretWord = new char[lengthOfTheWord];
+
+        public string GenerateWord()
+        {        
+            for (int i = 0; i < lengthOfTheWord; i++)
+            {
+                secretWord[i] = '_';
+            }
+
+            return theChosenWord;
+        }
+    }   
+}
