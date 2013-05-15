d15807ed3af31dfd579cb70572b1e10c8b6fdd1b
Svetlin Panov <svetlin.panov@gmail.com>
2013-05-15 21:07:00 +0300

    add ConsoleRender class
    add ConsoleRender class

---
+++ Hangman-3/ConsoleRender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HangmanGame
+{
+    public static class ConsoleRender
+    {
+        /// <summary>
+        /// prints on the console
+        /// </summary>
+        /// <param name="inString"></param>
+        public static void PrintOnConsole(string inString)
+        {
+            Console.WriteLine(inString);
+        }
+    }
+}
