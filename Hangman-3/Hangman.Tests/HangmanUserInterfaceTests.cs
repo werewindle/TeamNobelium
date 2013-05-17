7083bcd24b5f2ae3fc220dcbb74afe9f74589228
ms-nenova <ms.nenova@gmail.com>
2013-05-17 12:10:21 +0300

    HangemanUserInterfaceTests() - 2 tests added

---
+++ Hangman-3/Hangman.Tests/HangmanUserInterfaceTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HangmanGame;
+
+namespace Hangman.Tests
+{
+    [TestClass]
+    public class HangmanUserInterfaceTests
+    {
+        [TestMethod]
+        public void TestGenerateMessageMistakesNumber()
+        {
+            //string message = HangmanUserInterface.GuessMessage();
+            int mistakesNumber = 3;
+            string expected = string.Format("You won with {0} mistakes.\n", mistakesNumber);
+            string result = HangmanUserInterface.WonMessage(mistakesNumber);
+            Assert.AreEqual(expected, result);
+        }
+        
+        [TestMethod]
+        public void TestGenerateMessageWonWithCheating()
+        {            
+            int mistakesNumber = 3;
+            string expected = string.Format("You won with {0} mistakes but you have cheated. You are not allowed\n to enter into the scoreboard.\n", mistakesNumber);
+            string result = HangmanUserInterface.WonWithCheatingMessage(mistakesNumber);
+            Assert.AreEqual(expected, result);
+        }
+
+        /*[TestMethod]
+        public void TestGenerateMessageSecretWord()
+        {
+            string secretWord = "developer";
+            string expected = string.Format("The secret word is: {0}",secretWord);
+            string result = string.Format(HangmanUserInterface.SecretWordMessage(secretWord));
+            Assert.AreEqual(expected, result);
+        }*/
+    }
+}
